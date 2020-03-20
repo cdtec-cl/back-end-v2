90dce61d725e97508c35bcd8ca0faedb65400948
fercys <fercys.ramirez@birchmangroup.com>
2020-03-19 21:58:32 -0300

    agregando

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,7 @@ class Cors
     public function handle($request, Closure $next){
         return $next($request)
             //Url a la que se le dará acceso en las peticiones
-            // ->header("Access-Control-Allow-Origin", "https://dev.aparicioconsulting.com")
-            ->header("Access-Control-Allow-Origin", "https://api.dev.aparicioconsulting.com")
+            ->header("Access-Control-Allow-Origin", "*")
             //Métodos que a los que se da acceso
             ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
             //Headers de la petición
