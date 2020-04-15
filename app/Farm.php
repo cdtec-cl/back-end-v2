2533976fa16377bf825398133957c10e59b1df96
fercys <fercys.ramirez@birchmangroup.com>
2020-04-15 11:51:07 -0400

    agregando cambios

--- app/Farm.php
+++ app/Farm.php
@@ -10,7 +10,7 @@ class Farm extends Model
     protected $fillable = [
         'name', 'description','latitude', 'longitude','postalAddress', 'timeZone','webhook','id_account','id_wiseconn'
     ];
-  	protected $with = ['account','zones'];
+  	protected $with = ['account'];
     public function account()
     {
         return $this->hasOne(Account::class,'id','id_account');
