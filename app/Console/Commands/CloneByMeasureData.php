7b4c751bf3cc22a0925d5b5369aa9a0899f90739
Aquiles <aquilesmartinez@ingbasica.net>
2020-07-10 16:55:35 -0400

    agregando cambios

--- app/Console/Commands/CloneByMeasureData.php
+++ app/Console/Commands/CloneByMeasureData.php
@@ -89,7 +89,7 @@ class CloneByMeasureData extends Command
            $measures=Measure::whereIn('id', [59,60,61,62,63,64,65,66,67,68,69,70,71,343,344,339,340,341,343,344,342,423,424,425,426,427,428,429,429,430,705,3238,3239,3241,3243,3244,3245,3345,3351,3353,3374,3375,
                             2775,2776,3019,3020,3021,3022,3023,3024])->get(); //all();
 
-            $initTime=Carbon::now(date_default_timezone_get())->subDays(10)->format('Y-m-d');
+            $initTime=Carbon::now(date_default_timezone_get())->subDays(5)->format('Y-m-d');
             $endTime=Carbon::now(date_default_timezone_get())->addDays(1)->format('Y-m-d');
             $this->info("==========Fecha Inicio (".$initTime." elementos)");
             $this->info("==========Fecha Finalizacion (".$endTime." elementos)");
