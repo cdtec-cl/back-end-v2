ae803f8591ab0e1732e324d7a07b4e61ae0303d9
Aquiles <aquilesmartinez@ingbasica.net>
2020-11-13 19:36:41 -0300

    agregando cambios

--- app/Console/Commands/CloneByFarmMeasureData.php
+++ app/Console/Commands/CloneByFarmMeasureData.php
@@ -69,52 +69,9 @@ class CloneByFarmMeasureData extends Command
                     $cloningErrors=CloningErrors::where("elements","/farms/id/measures")->get();
                     $currentRequestElement='/farms/id/measures';
                     $id_wiseconn=$farm->id_wiseconn;
-                    // if(count($cloningErrors)>0){
-                    //     $this->info("Cloning Error");
-                    //     foreach ($cloningErrors as $key => $cloningError) {
-                    //         try{
-                    //             if($key % 3 == 0){
-                    //                 $this->info("sleep(2)");
-                    //                 sleep(2);
-                    //             }
-                    //             $valor= $key % 3;
-                    //             $this->info($valor);                  
-                    //             $this->info("requestWiseconn()");
-                    //             $this->info($cloningError->uri);
-                    //             $measuresResponse = $this->requestWiseconn('GET',$cloningError->uri);
-                    //             $measures=json_decode($measuresResponse->getBody()->getContents());
-                    //             foreach ($measures as $key => $value) {
-                    //                 $measure=Measure::where("id_wiseconn",$value->id)->first();
-                    //                 if(!is_null($measure)){
-                    //                     if($value->id[0].$value->id[1]== "1-"){                                      
-                    //                         if(isset($value->lastData)&&!is_null($value->lastData)&&isset($value->lastDataDate)&&!is_null($value->lastDataDate)){
-                    //                             $measureData=new MeasureData();
-                    //                             $measureData->value=isset($value->lastData)?$value->lastData:null;
-                    //                             $measureData->time=isset($value->lastDataDate)?$value->lastDataDate:null;
-                    //                             $measureData->id_measure=$measure->id;
-                    //                             $measureData->save();
-                    //                             $this->info("New MeasureData id:".$measureData->id);
-                    //                         }
-                    //                     }else{
-                    //                         $this->info("Elemento no registrado por id:".$value->id);
-                    //                     }
-                    //                 }
-                    //                 $cloningError->delete();
-                    //             }
-                    //         }catch (\Exception $e) {
-                    //             $this->error("Error:" . $e->getMessage());
-                    //             // $error=explode("message", $e->getMessage(), 2); 
-                    //             // $this->error("EN ESTA PINGA:" . $error[0]);
-                    //             // $this->error("EN ESTA PINGA:" . $error[1]);
-                    //             $this->error("Linea:" . $e->getLine());                                
-                    //             $this->error("currentRequestUri:" . $cloningError->uri);
-
-                    //         }
-                    //     }
-                    // }else{
                         try{
                             if($key % 3 == 0){
-                                sleep(1);
+                                sleep(2);
                             }
                             $executionStartTime = microtime(true);
                             $measuresResponse = $this->requestWiseconn('GET',$currentRequestUri);
@@ -125,11 +82,11 @@ class CloneByFarmMeasureData extends Command
                             $executionStartTime2 = microtime(true);
                             $arrayMeasures = []; 
                             foreach ($measures as $key => $value) {
-                                $measure2222=Measure::where("id_wiseconn",$value->id)->first();
-                                if(!is_null($measure2222)){
+                                $measure=Measure::where("id_wiseconn",$value->id)->first();
+                                if(!is_null($measure)){
                                     if($value->id[0].$value->id[1]== "1-"){
                                         $arrayMeasures[] = [
-                                            'id_measure' => $measure2222->id, 
+                                            'id_measure' => $measure->id, 
                                             'value'      => isset($value->lastData)?$value->lastData:null,
                                             'time'       => isset($value->lastDataDate)?$value->lastDataDate:null,
                                             'created_at' => $fechaData,
@@ -148,41 +105,15 @@ class CloneByFarmMeasureData extends Command
                             $this->error("Error:" . $e->getMessage());
                             $this->error("Linea:" . $e->getLine());
                             $this->error("currentRequestUri:" . $currentRequestUri);
-                            $this->info('Se ejecuto');   
-                            sleep(2);
-                            $this->info($currentRequestUri . "CURRENT");
-                            $measuresResponse2 = $this->requestWiseconn('GET',$currentRequestUri);
-                            $measures2=json_decode($measuresResponse2->getBody()->getContents());
-                            $this->info("MEASURESRESPONSE");
-                            $this->info($measuresResponse2);
-                            $this->info("MEASURES");
-                            $this->info($measures2);
-                            $arrayMeasures2 = []; 
-                            foreach ($measures2 as $key => $value) {
-                                $measure2222=Measure::where("id_wiseconn",$value->id)->first();
-                                if(!is_null($measure2222)){
-                                    if($value->id[0].$value->id[1]== "1-"){
-                                        $arrayMeasures2[] = [
-                                            'id_measure' => $measure2222->id, 
-                                            'value'      => isset($value->lastData)?$value->lastData:null,
-                                            'time'       => isset($value->lastDataDate)?$value->lastDataDate:null,
-                                            'created_at' => $fechaData,
-                                            'updated_at' => $fechaData
-                                        ];
-                                    }
-                                }
-
-                            }     
-                            DB::table('measure_data')->insert($arrayMeasures2);
-                            $this->info('Se Agrego la data');
+                            $this->info('Se ejecuto');                         
                             $this->info($currentRequestUri);
-                           /* if(is_null(CloningErrors::where("elements",$currentRequestElement)->where("uri",$currentRequestUri)->where("id_wiseconn",$id_wiseconn)->first())){
+                            if(is_null(CloningErrors::where("elements",$currentRequestElement)->where("uri",$currentRequestUri)->where("id_wiseconn",$id_wiseconn)->first())){
                                 $cloningError=new CloningErrors();
                                 $cloningError->elements=$currentRequestElement;
                                 $cloningError->uri=$currentRequestUri;
                                 $cloningError->id_wiseconn=$id_wiseconn;
                                 $cloningError->save();
-                            }*/
+                            }
                         }
                     // }
                 } catch (\Exception $e) {
