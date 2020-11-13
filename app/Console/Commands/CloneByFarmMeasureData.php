1ffda79ff3e20fd19a8dfb82c784a2eec6ab9eb1
Aquiles <aquilesmartinez@ingbasica.net>
2020-11-13 19:05:53 -0300

    agregando cambios

--- app/Console/Commands/CloneByFarmMeasureData.php
+++ app/Console/Commands/CloneByFarmMeasureData.php
@@ -114,7 +114,7 @@ class CloneByFarmMeasureData extends Command
                     // }else{
                         try{
                             if($key % 3 == 0){
-                                sleep(2);
+                                sleep(1);
                             }
                             $executionStartTime = microtime(true);
                             $measuresResponse = $this->requestWiseconn('GET',$currentRequestUri);
@@ -149,11 +149,12 @@ class CloneByFarmMeasureData extends Command
                             $this->error("Linea:" . $e->getLine());
                             $this->error("currentRequestUri:" . $currentRequestUri);
                             $this->info('Se ejecuto ');   
-                            sleep(3);
-                            $this->info($currentRequestUri);
+                            sleep(2);
+                            $this->info($currentRequestUri . "CURRENT");
                             $measuresResponse2 = $this->requestWiseconn('GET',$currentRequestUri);
                             $measures2=json_decode($measuresResponse2->getBody()->getContents());
-                            $this->info($measuresResponse2);
+                            $this->info($measures2 . "MEASURES");
+                            $this->info($measuresResponse2 . "MEASURESRESPONSE");
                             $arrayMeasures2 = []; 
                             foreach ($measures2 as $key => $value) {
                                 $measure2222=Measure::where("id_wiseconn",$value->id)->first();
@@ -185,6 +186,7 @@ class CloneByFarmMeasureData extends Command
                 } catch (\Exception $e) {
                     $this->error("Error:" . $e->getMessage());
                     $this->error("Linea:" . $e->getLine());
+                    $this->info('Se Agrego la data22222');
                     $this->error("currentRequestUri:" . $currentRequestUri);
                     if(is_null(CloningErrors::where("elements",$currentRequestElement)->where("uri",$currentRequestUri)->where("id_wiseconn",$id_wiseconn)->first())){
                         $cloningError=new CloningErrors();
