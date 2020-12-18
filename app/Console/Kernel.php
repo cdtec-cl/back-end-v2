88714f0ea12be7a19315b53be77bdf6785ddfc42
Aquiles <aquilesmartinez@ingbasica.net>
2020-12-18 08:28:13 -0300

    actualiando riegos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -79,7 +79,7 @@ class Kernel extends ConsoleKernel
         // php artisan clonebyfarm:measures:run
       //   $schedule->command('clonebyfarm:measures:run')->everyTenMinutes();
         // php artisan clonebyfarm:irrigations:volumes:run
-      //  $schedule->command('clonebyfarm:irrigations:volumes:run')->everyTenMinutes();
+        $schedule->command('clonebyfarm:irrigations:volumes:run')->everyTenMinutes();
         // php artisan clonebyfarm:realirrigations:volumes:run
         $schedule->command('clonebyfarm:realirrigations:volumes:run')->everyTenMinutes();
         // php artisan clonebyfarm:alarms:run
@@ -97,7 +97,7 @@ class Kernel extends ConsoleKernel
         // php artisan clonebynode:measures:run
         // $schedule->command('clonebynode:measures:run')->everyFiveMinutes();
         // php artisan clonebyirrigation:realirrigations:run
-     //   $schedule->command('clonebyirrigation:realirrigations:run')->everyFiveMinutes();
+        $schedule->command('clonebyirrigation:realirrigations:run')->everyTenMinutes();
         // php artisan clonebypumpsystem:irrigations:volumes:run
        // $schedule->command('clonebypumpsystem:irrigations:volumes:run')->everyFiveMinutes();
         // php artisan clonebypumpsystem:realirrigations:volumes:run
