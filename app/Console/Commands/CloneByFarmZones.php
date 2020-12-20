df6ec0143b3927faa33ab41a1b7dc16ce14a7c46
Aquiles <aquilesmartinez@ingbasica.net>
2020-12-20 17:04:55 -0300

    actualizando zonas

--- app/Console/Commands/CloneByFarmZones.php
+++ app/Console/Commands/CloneByFarmZones.php
@@ -120,7 +120,7 @@ class CloneByFarmZones extends Command
     {
         try { 
             // $farms=Farm::all();
-            $farms=Farm::whereIn('id_wiseconn', [2733,1423])->get();
+            $farms=Farm::whereIn('id_wiseconn', [2014,2163,2284])->get();
             foreach ($farms as $key => $farm) {
                 if($farm->active_cloning==1){
                     try {
