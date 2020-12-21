ac2360157d132179f29da3e6d238934dba7c9b0f
Aquiles <aquilesmartinez@ingbasica.net>
2020-12-21 09:24:56 -0300

    340 farm

--- app/Console/Commands/CloneByFarmMeasures.php
+++ app/Console/Commands/CloneByFarmMeasures.php
@@ -268,7 +268,7 @@ class CloneByFarmMeasures extends Command
         try{
            // $farms=Farm::all();
 
-            $farms=Farm::whereIn('id_wiseconn', [2063])->get();  
+            $farms=Farm::whereIn('id_wiseconn', [340])->get();  
             
 
             foreach ($farms as $key => $farm) {
